ab9b62efdd7b69a5e54f40b6b997700c55cc6ad0
Hugo <hugovk@users.noreply.github.com>
2018-10-31 11:58:11 +0200

    Use context manager

--- mass_shoot_bot.py
+++ mass_shoot_bot.py
@@ -40,12 +40,14 @@ def load_yaml(filename):
     access_token_secret: TODO_ENTER_YOURS
     wordnik_api_key: TODO_ENTER_YOURS
     """
-    f = open(filename)
-    data = yaml.safe_load(f)
-    f.close()
-    if not data.viewkeys() >= {
-            'access_token', 'access_token_secret',
-            'consumer_key', 'consumer_secret'}:
+    with open(filename) as f:
+        data = yaml.safe_load(f)
+
+    keys = data.viewkeys() if sys.version_info.major == 2 else data.keys()
+    if not keys >= {
+        'access_token', 'access_token_secret',
+        'consumer_key', 'consumer_secret'
+    }:
         sys.exit("Twitter credentials missing from YAML: " + filename)
 
     if 'last_shooting' not in data:
