eff573ffe708de2d0c465945b77846aa2bd356ed
Hugo <hugovk@users.noreply.github.com>
2018-11-10 22:19:56 +0200

    Add helper scripts

---
+++ crontask.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+cd ~/github/mass_shoot_bot/
+./update.sh
+./run.sh
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+python3 mass_shoot_bot.py --yaml ~/bin/data/mass_shoot_bot.yaml --csv data --no-web -x
